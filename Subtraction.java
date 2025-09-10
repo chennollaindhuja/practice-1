d1c5099ca9a2bb2fc28939405526af0b7ea82896
chennollaindhuja <chennollaindhujareddy@gmail.com>
2025-09-10 14:28:51 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1 = 20; // First number
+        int num2 = 10; // Second number
+
+        int difference = num1 - num2; // Subtraction operation
+
+        System.out.println("Difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
